037011ed6359590cf3f1ce95d175a7b43d92225b
anhong <anhonghe@gmail.com>
2022-11-20 19:02:27 -0800

    update from PyCharm

--- src/calc_sub_window.py
+++ src/calc_sub_window.py
@@ -1,6 +1,6 @@
 from PyQt6.QtGui import *
 from PyQt6.QtCore import *
-from PyQt6.QtWidgets import QGraphicsProxyWidget, QMenu
+from PyQt6.QtWidgets import *
 
 from statenodewidget import StateNodeWidget
 from node import *
