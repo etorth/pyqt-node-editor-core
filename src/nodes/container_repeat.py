670f02f71590c852338f2db910b9c7b1f52215fd
anhong <anhonghe@gmail.com>
2022-11-21 15:32:41 -0800

    update from PyCharm

---
+++ src/nodes/container_repeat.py
@@ -0,0 +1,68 @@
+from PyQt6.QtGui import *
+from PyQt6.QtCore import *
+from PyQt6.QtWidgets import *
+
+from qdnode import *
+from qdutils import *
+from qdnodecontentgfx import *
+
+
+class _ContainerContentGfx_repeat(QD_NodeContentGfx):
+    def initUI(self):
+        self.hbox = QHBoxLayout()
+
+        self.choice = QComboBox()
+        self.choice.addItems(["为真时", "为假时", "一直"])
+
+        self.label1 = QLabel('重复')
+
+        self.times = QLineEdit()
+        self.times.setValidator(QIntValidator())
+        self.times.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        self.label2 = QLabel('次')
+
+        self.hbox.addWidget(self.choice)
+        self.hbox.addWidget(self.label1)
+        self.hbox.addWidget(self.times)
+        self.hbox.addWidget(self.label2)
+
+        self.vbox = QVBoxLayout(self)
+        self.vbox.addLayout(self.hbox)
+
+        self.ops = QLabel('操作')
+        self.vbox.addWidget(self.ops)
+
+
+class _ContainerContent_repeat(QD_NodeContent):
+    NodeContentGfx_class =_ContainerContentGfx_repeat
+
+
+@utils.register_opnode
+class _Container_repeat(QD_Node):
+    icon = "icons/editor.png"
+    op_type = OPS_CONTAINER
+    op_title = "重复"
+
+    NodeContent_class = _ContainerContent_repeat
+
+
+    def __init__(self, scene):
+        super().__init__(scene, inputs=[2], outputs=[3])
+        self.eval()
+
+
+    def evalImplementation(self):
+        u_value = 1 # hack
+        s_value = int(u_value)
+        self.value = s_value
+        self.markDirty(False)
+        self.markInvalid(False)
+
+        self.markDescendantsInvalid(False)
+        self.markDescendantsDirty()
+
+        self.gfx.setToolTip("")
+
+        self.evalChildren()
+        return self.value
