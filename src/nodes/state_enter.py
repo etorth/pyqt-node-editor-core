d788b26a1acfb712c284726d55302b6ee8a700a8
anhong <anhonghe@gmail.com>
2022-11-22 21:53:02 -0800

    update from PyCharm

--- src/nodes/state_enter.py
+++ src/nodes/state_enter.py
@@ -18,20 +18,20 @@ class _StateContent_enter(QD_NodeContent):
     NodeContentGfx_class =_StateContentGfx_enter
 
     def serialize(self):
-        res = super().serialize()
-        res['value'] = self.gfx.label.text()
-        return res
+        data = super().serialize()
+        data['value'] = self.gfx.label.text()
+        data['op_code'] = self.__class__.op_code
+        return data
 
 
     def deserialize(self, data, hashmap={}):
-        res = super().deserialize(data, hashmap)
+        data = super().deserialize(data, hashmap)
         try:
-            value = data['value']
-            self.gfx.label.setText(value)
-            return True & res
+            self.gfx.label.setText(data['value'])
+            return True & data
         except Exception as e:
             utils.dumpExcept(e)
-        return res
+        return data
 
 
 @utils.register_opnode
