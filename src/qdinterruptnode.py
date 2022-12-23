45e0ef2c3a0710b32570fd686e5f8e91497bc259
anhong <anhonghe@gmail.com>
2022-12-22 19:39:24 -0800

    update from PyCharm

--- src/qdinterruptnode.py
+++ src/qdinterruptnode.py
@@ -11,4 +11,4 @@ from qdstatenode import QD_StateNode
 class QD_InterruptNode(QD_StateNode):
     def __init__(self, scene: 'QD_QuestScene', sockets: set = {SocketType.In, SocketType.Out_True}):
         super().__init__(scene, sockets)
-        self._title = '中断节点'
+        self.title = '中断节点'
