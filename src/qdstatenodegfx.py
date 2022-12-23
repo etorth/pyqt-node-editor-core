79ccd0df77a6fd63aa909eac8c668b5df2677bfd
anhong <anhonghe@gmail.com>
2022-12-23 13:17:02 -0800

    update from PyCharm

--- src/qdstatenodegfx.py
+++ src/qdstatenodegfx.py
@@ -117,7 +117,7 @@ class QD_StateNodeGfx(QGraphicsItem):
         self._brush_background = QBrush(QColor("#E3212121"))
 
         self._icons = QImage("icons/status_icons.png")
-        self._image = QImage("icons/dst.png")
+        self._image = QImage("icons/src.png")
 
     def onSelected(self):
         """Our event handling when the node was selected"""
